fe4508182fb24937ab2be937dd5a2d9349860f82
Daniel <itzdanielmichael@gmail.com>
2021-05-14 16:04:05 -0400

    update App.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,36 @@
 <template lang="">
   <div>
-    About
+    <ul class="meds-list" v-for="med in allMeds" :key="med.id">
+      <li class="med-card">{{ med.fields.name }}</li>
+    </ul>
   </div>
 </template>
 <script>
+import { getAllMeds } from '../services/allMeds';
+
 export default {
   name: 'About',
+  data() {
+    return {
+      allMeds: [],
+    };
+  },
+  mounted() {
+    const fetchMeds = async () => {
+      const medData = await getAllMeds();
+      this.allMeds = medData;
+    };
+    fetchMeds();
+  },
 };
 </script>
-<style lang=""></style>
+<style>
+.meds-list {
+  /* RESET UL */
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+.med-card {
+}
+</style>
